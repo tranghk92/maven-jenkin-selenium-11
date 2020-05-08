e444f59d2e98aaca701f5d600f45b45948d43c24
kimtrang <kimtrang@192.168.1.25>
2020-05-08 19:12:44 +0700

    test

---
+++ src/test/java/selenium/runTestcaseDemoFF.java
@@ -0,0 +1,45 @@
+package selenium;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class runTestcaseDemoFF {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.get("http://demo.guru99.com/V4/");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	@Test
+	public void TC_01_DemoRunOnJenkins() {
+		String loginTitle = driver.getTitle();
+		Assert.assertEquals("Guru99 Bank Home Page", loginTitle);
+		WebElement userTxt = driver.findElement(By.xpath("//input[@name='uid']"));
+		Assert.assertTrue(userTxt.isDisplayed());
+	}
+  @Test
+  public void TC_02_getURL() {
+	  String loginUrl = driver.getCurrentUrl();
+	  Assert.assertEquals("http://demo.guru99.com/V4/", loginUrl);
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
